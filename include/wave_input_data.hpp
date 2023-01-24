8d68f3c4e9942ffebe7888b5ad944088a832c8fc
BaretBuckley <thejocky10000@gmail.com>
2023-01-24 12:21:45 -0500

    Modified process of input images

--- include/wave_input_data.hpp
+++ include/wave_input_data.hpp
@@ -18,47 +18,27 @@ namespace wfc::input {
 
 
     class InputGrid {
-        size_t width_, height_;
-        Tile *data_;
+
+        uint8_t* data;
+        uint32_t width_, height_, channels_;
 
         public:
 
         InputGrid(size_t width, size_t height);
 
-        const expr TileSize() {return sizeof(Tile);}
-
         size_t width() {return width_;}
         size_t height() {return height_;}
 
-        void setTile(size_t x, size_t y, Tile tile) {data[y*width_ + x] = tile;};
-        Tile getTile(size_t x, size_t y) {return data[y*width_ + x];}
-        
-        Tile *operator[](int i) {return data_[i*width_];}
+        void setTile(size_t x, size_t y, Tile tile);
+        Tile getTile(size_t x, size_t y);
+
+        // Tile *operator[](int i) {return data_[i*width_];}
     }
 
-    class RuleSet {
 
 
-        int states_;
-        float* wieghts_;
-        std::vector<DynamicBitset[4]> rules_;
-
-        public:
-
-        RuleSet() : states_(0) {}
-        ~RuleSet() {}
-
-        bool addImage(InputImage image);
-        bool addImage(const char* path);
-        bool addRulesFromFile(const char* path);
-        bool reset() {rules_.clear();}
-
-        bool loadToFile(const char* path);
-
-        void getWeight(int state) {return weights_[state];}
-        void getRule(int state, Wave::Direction direction);
-        
-    };
+        // void setTile(size_t x, size_t y, Tile tile) override {data[y*width_ + x] = tile;}
+        // Tile getTile(size_t x, size_t y) override {return data[y*width_ + x];}
 
     class ImageLoader {
         std::map<Pixel, Tile> encodingMap_;
@@ -70,12 +50,48 @@ namespace wfc::input {
         ImageLoader() {}
         ~ImageLoader() {}
 
-        void setImplicitEncoding(bool value) {implicitEncoding_ = true;}
+        void loadRulesFromImage()
 
         InputGrid* loadImage(const char* path);
+
+        Tile encodePixel(Pixel pixel) {return encodingMap_[pixel];}
+        Pixel decodeTile(Tile tile) {return decodingMap_[tile];}
         
 
     };
+
+
+
+    class RuleSet {
+
+
+        int states_;                   // Number of states currently in ruleset
+        std::vector<float> wieghts_;   // Weights of each state
+        std::vector<uint32_t> counts_; // Number of each state encountered in input
+        size_t processedStates_;       // Total number of states processed by ruleset 
+        std::vector<DynamicBitset[4]> rules_;
+
+        public:
+
+        RuleSet() : states_(0) {}
+        ~RuleSet() {}
+
+        bool addInput(InputGrid grid, ImageLoader& loader);
+        bool addImage(uint8_t* image, ImageLoader& loader);
+        bool addImageFromFile(const char* path, ImageLoader& loader);
+
+
+        bool addRulesFromFile(const char* path);
+        bool reset() {rules_.clear();}
+
+        bool loadToFile(const char* path);
+
+        void getWeight(int state) {return weights_[state];}
+        void getRule(int state, Wave::Direction direction);
+        
+    };
+
+    
 
 
 
--- source/wave_input_data.cpp
+++ source/wave_input_data.cpp
@@ -3,14 +3,39 @@
 namespace wfc::input {
 
 
-    // bool RuleSet::addImage(InputImage image) {
+    bool RuleSet::addImage(InputImage image) {
         
-    // }
+    }
 
 
-    // bool RuleSet::loadImage(const char* path) {
+    bool RuleSet::addInput(InputGrid grid, ImageLoader& loader) {
+        uint32_t state;
+        for (int x = 0; x < grid.width(); x++) {
+            state = grid.getTile(x, 0);
+            if (state >) 
+        }
+
+        
+
+
+
+    }
+    // bool RuleSet::loadRulesFromFile(const char* path);
+
+    // void getRule(int state, Wave::Direction direction);
+
+
+
+    ImageLoader::loadRulesFromImage(unsigned char* image) {
+
+
+    }
+
+
+    // InputGrid* loadImage(const char* path) {
     //     int width, height, nrChannels;
     //     unsigned char* data;
+    //     InputGrid* grid;
 
     //     data = stbi_load(file, &width, &height, &nrChannels, 0);
     //     if (!data) {std::cerr << "ERRROR OPENING TEXTURE: " << file << "\n"; return false;}
@@ -18,27 +43,14 @@ namespace wfc::input {
     //     width_ = width;
     //     height_ = height;
 
-        
+    //     grid = new InputGrid(width, height);
 
+    //     for (int x = 0; x < width; x++) {
+    //         for (int y = 0; y < height; y++) {
 
-
+    //         }
+    //     }
     // }
-    // bool RuleSet::loadRulesFromFile(const char* path);
-
-    // void getRule(int state, Wave::Direction direction);
-
-
-    ImageLoader::loadImage(const char* path) {
-        int width, height, nrChannels;
-        unsigned char* data;
-
-        data = stbi_load(file, &width, &height, &nrChannels, 0);
-        if (!data) {std::cerr << "ERRROR OPENING TEXTURE: " << file << "\n"; return false;}
-
-        width_ = width;
-        height_ = height;
-
-    }
 
 
 
